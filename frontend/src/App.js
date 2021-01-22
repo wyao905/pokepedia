6996bc98338cc0e30f52c22ce00ec3669d03fd70
wyao905 <warrenyca905@gmail.com>
2021-01-21 19:27:44 -0500

    created frontend portion using create react app

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Header from './components/header.js'
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Header(props) {
+    return <div>
+    </div>
+}
+
+export default Header
